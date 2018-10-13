7bd4294d7c31cd6a8774f9856fd2a4f1e3d61ab0
Haas Julian <j.haas03@gmail.com>
2018-10-13 14:45:59 +0200

    Object Literal Syntax, Factories, Constructor Fucntions, Constructor Property, Implementierung von Objekten, Anzeige der erstellenden Funktion. Stoff aus der Stunde des 11.10.2018

--- app.js
+++ app.js
@@ -1,18 +1,59 @@
-let logGreeting1 = function(){
-    console.log("Hallo 1");
+// OBject Literal Syntax
+const circle = {
+    radius: 1,
+    location:{
+        x:1,
+        y:1
+    },
+    draw: function(){
+        console.log('draw1');
+    }
 };
 
-let logGreeting2 = function(){
-    console.log("Hallo 2");
-};
+circle.draw();
 
-let logGreeting3 = function(){
-    console.log("Hallo 3");
-};
+//Factories
+function createCircle(radius){
+    return{
+        radius,
+        draw1: function(){
+            console.log('draw2');
+        }
+    };
+}
+const circle1 = createCircle(1);
+circle1.draw1();
 
 
+//Konstruktor
+function Circle(radius){
+    console.log('this', this);
+    this.radius= radius;
+    this.draw=function(){
+      console.log('draw');
+    };
+}
 
-logGreeting1();
-setTimeout(logGreeting2, 1000);
-logGreeting3();
+const another = new Circle(1);
+
+/* Wenn man new weglässt
+function Circle1(radius){
+    console.log('this', this);
+    this.radius= radius;
+    this.draw=function(){
+        console.log('draw');
+    };
+}
+
+const another1 =  Circle1(1);
+*/
+// Objekte Implementieren
+let x={};
+let name ='hallo';
+let ok=false;
+let total=1;
+
+// Anzeigen der erstellenden Funktion
+let name5='susi';
+console.log('name = ', name5, name5.constructor);
 
